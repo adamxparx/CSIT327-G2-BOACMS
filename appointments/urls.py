7ab8472b79b86136073d925163e61894888802f1
Adam Cloyd H. Esparcia <adamcloydesparcia@gmail.com>
2025-12-11 23:00:10 +0800

    feature(appointments): add claimed appointments for resident confirmation to claimed documents

--- appointments/urls.py
+++ appointments/urls.py
@@ -4,11 +4,11 @@ from .views import RequirementsView
 
 urlpatterns = [
     path('appointments/', views.appointments, name='appointments'),
-    path('staff_appointments/', views.staff_appointments, name='staff_appointments'),
     path('approved_appointments/', views.approved_appointments, name='approved_appointments'),
     path('pending_appointments/', views.pending_appointments, name='pending_appointments'),
     path('cancelled_appointments/', views.cancelled_appointments, name='cancelled_appointments'),
     path('completed_appointments/', views.completed_appointments, name='completed_appointments'),
+    path('claimed_appointments/', views.claimed_appointments, name='claimed_appointments'),
     path('certification/', views.create_appointment, name='certification'),
     path('confirmation/<int:appointment_id>/', views.confirmation, name='confirmation'),
     path('cancel/<int:appointment_id>/', views.cancel_appointment, name='cancel_appointment'),
